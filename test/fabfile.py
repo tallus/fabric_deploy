7cdcc0e0af9dda9487d7d2953dafe37b2523e8d3
paulm <paulm@freegeek.org>
2013-07-25 14:25:51 -0700

    new file:   .gitignore 	new file:   test/fabfile.py

---
+++ test/fabfile.py
@@ -0,0 +1,22 @@
+from fabric.api import *
+from fabric.contrib.console import confirm
+
+env.hosts = ['bruno']
+
+def hello(name='world'):
+    print("Hello %s!" % name)
+    
+def testf(tfile = 'foo'):
+    with settings(warn_only=True):
+        result=local("ls " +  tfile, capture=True)
+    if result.failed and not confirm("Tests failed. Continue anyway?"):
+        abort("Aborting at user request.")
+    print('output: ' + result)
+
+def testr(rdir='/srv/tsbackup'):
+    result = run("ls " + rdir, quiet=True)
+    if result.failed:
+        abort("Fail!")
+    else:
+        print('output: ' + result)
+    
